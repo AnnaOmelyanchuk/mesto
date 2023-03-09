20490a80e13e18ee1d4dd9f1c3c36da101cab31c
Your Name <you@example.com>
2023-03-10 00:08:30 +0300

    second_try

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,17 +1,19 @@
-let onclickTargetPopup;
-let nameInput;
-let jobInput;
-
+let nameInput = document.querySelector('.popup__input_text_name');
+let jobInput = document.querySelector('.popup__input_text_caption');
+let profileName = document.querySelector('.profile__name');
+let profileCaption = document.querySelector('.profile__caption');
+const editButton = document.querySelector('button');
+const closeButton = document.querySelector('.popup__close-btn');
+const formElement = document.querySelector('.popup__form');
 
 function openForm() {
-  if (onclickTargetPopup.classList[0] == 'profile__edit-button') {
-    document.querySelector(".popup").classList.add("popup_opened");
-  }
+  document.querySelector(".popup").classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileCaption.textContent;
 }
 
 function closeForm() {
-  if (onclickTargetPopup.classList[0] == 'popup__close-btn')
-    document.querySelector(".popup").classList.remove("popup_opened");
+  document.querySelector(".popup").classList.remove("popup_opened");
 }
 
 
@@ -20,14 +22,14 @@ function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
-  nameInput = document.querySelector('.popup__input-name');
-  jobInput = document.querySelector('.popup__input-caption');
-  document.querySelector('.profile__name').innerText = nameInput.value;
-  document.querySelector('.profile__caption').innerText = jobInput.value;
-  document.querySelector(".popup").classList.remove("popup_opened");
+
+  profileName.textContent = nameInput.value;
+  profileCaption.textContent = jobInput.value;
+  closeForm();
 }
 
 
-document.addEventListener('submit', handleFormSubmit);
-document.addEventListener("click", event => { onclickTargetPopup = event.target; openForm(); closeForm() });
+formElement.addEventListener('submit', handleFormSubmit);
+editButton.addEventListener("click", event => openForm());
+closeButton.addEventListener("click", event => closeForm());
 
